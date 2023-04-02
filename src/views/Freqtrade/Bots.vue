8e1d7d591c3a1f1db7ab9caa864c7daae6d5850c
SinoLewis <sinolewis@gmail.com>
2023-04-03 00:08:12 +0300

    UI Fix

--- src/views/Freqtrade/Bots.vue
+++ src/views/Freqtrade/Bots.vue
@@ -9,17 +9,17 @@
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Freqtrade Login</ion-title>
+          <!-- <login-modal
+            v-if="!small"
+            ref="loginModal"
+            class="mt-2"
+            login-text="Add new bot"
+          /> -->
         </ion-toolbar>
       </ion-header>
       <div class="home">
         <div class="d-flex justify-content-center">
           <bot-list />
-          <login-modal
-            v-if="!small"
-            ref="loginModal"
-            class="mt-2"
-            login-text="Add new bot"
-          />
         </div>
         <hr />
         <!-- <img alt="Freqtrade logo" src="../assets/freqtrade-logo.png" width="450px" class="my-5" /> -->
@@ -38,7 +38,9 @@
 
         <p>Have fun - <i>wishes you the Freqtrade team</i></p> -->
       </div>
-      <ExploreContainer name="This page allows you to control your trading bot." />
+      <ExploreContainer
+        name="This page allows you to control your trading bot."
+      />
     </ion-content>
   </ion-page>
 </template>
